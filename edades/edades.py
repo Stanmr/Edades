f4abee8f36fb507f668bbb1b274e35e3df4180b8
Stanmr <pachuquin_8@hotmail.com>
2016-10-12 12:15:11 -0500

    Agrega funcionalidad de mostrar un mensaje dependiendo la edad que tenemos

--- edades/edades.py
+++ edades/edades.py
@@ -1,18 +1,19 @@
 # -*- coding: utf-8 -*-
 class Edades:
-	def mensaje(self, edad):
-		try:
-			if(edad<0):
-				return 'no existes'
-			elif(edad <13):
-				return u'eres niño'
-			elif(edad < 18):
-				return 'Eres un adolescente'
-			elif(edad < 65):
-				return 'Eres adulto'
-			elif(edad < 120):
-				return 'Eres adulto mayor'
-			else:
-				return 'Eres Mumm-Ra'
-		except Exception, e:
-			raise e
+
+    def mensaje(self, edad):
+        try:
+            if(edad < 0):
+                return 'no existes'
+            elif(edad < 13):
+                return u'eres niño'
+            elif(edad < 18):
+                return 'Eres un adolescente'
+            elif(edad < 65):
+                return 'Eres adulto'
+            elif(edad < 120):
+                return 'Eres adulto mayor'
+            else:
+                return 'Eres Mumm-Ra'
+        except Exception:
+            return 'Error'
